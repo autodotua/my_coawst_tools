f65b4d017f67d19e12aae1bb847fcea7a14bbc0e
autodotua <autodotua@outlook.com>
2022-05-25 14:58:03 +0800

    基本完成了水交换

--- show_water_exchange.m
+++ show_water_exchange.m
@@ -1,19 +1,56 @@
-function show_water_exchange(dye,zones,count)
-    % arguments
-    %     dye(:,:) cell
-    %     zones(:,:) double
-    %     count(1,1) double
-    % end
+function show_water_exchange
+    configs
+    dye=evalin('base', 'dye_02');
+    s=size(dye);
+    z=zeros(s(1:3));
+    m=ncread(fullfile(roms.project_dir,roms.input.grid),'mask_rho');
+    for x=1:s(1)
+        for y=1:s(2)
+            if m(x,y)
+                if x<78
+                    z(x,y,:)=1;
+                elseif x<164
+                    if y>92
+                        z(x,y,:)=2;
+                    elseif y<42
+                        z(x,y,:)=3;
+                    else
+                        z(x,y,:)=4;
+                    end
+                else
+                    z(x,y,:)=5;
+                end
+            end
+        end
+    end
+    %figure(1); [X,Y]=meshgrid(1:s(1),1:s(2));    pcolorjw(X,Y,z(:,:,1).') %绘制区域示意图
+    show_water_exchange_core(dye,z,5)
+end
+function show_water_exchange_core(dye,zones,count)
+    arguments
+        dye(:,:,:,:) double
+        zones(:,:,:) double
+        count(1,1) double
+    end
     configs
     v=get_volumes;
     s=size(dye);
-
-
-
-    r=zeros(count,s(4));
-    for t=1:s(4)
-
+    dye(isnan(dye))=0;%将NaN转为0方便计算
+    sum_dye_w=sum(dye(:,:,:,1).*v,'all'); %初始时刻的示踪剂总质量
+    figure(2);
+    for z=1:count
+        weights=zeros(s(4),1);
+        for t=1:s(4)
+            dye_w=dye(:,:,:,t).*v; %当前时间，示踪剂在不同网格内的质量
+            dye_sum_w_in_zone=sum(dye_w(zones==z),'all'); %当前时间当前区域内示踪剂的总质量
+            weights(t)=dye_sum_w_in_zone/sum_dye_w; %该时间该区域内的示踪剂质量占初始时刻总质量的百分比
+        end
+        plot(weights,'LineWidth',1)
+        hold on
     end
+    axis([0,s(4),0,1]);
+    legend(num2str((1:count).'))
+
 end
 
 function r=get_volumes %获取每一个网格的水的体积
@@ -49,9 +86,9 @@ function r=get_volumes %获取每一个网格的水的体积
 
     r=zeros(size_xy(1),size_xy(2),size_z);
     for i=1:size_z
-        real_height=h*(sc_w(i+1)-sc_w(i));
+        real_height=h*(sc_w(i+1)-sc_w(i));%计算真实高度
         real_height(~mask)=0;
         r(:,:,i)=real_height;
     end
-r=r.*area;
+    r=r.*area;
 end
